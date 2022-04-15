9a4d33e6c96351e8bbff99fe8dd90e6210bd4b5a
Emilio Gonzales <emiliog1184@gmail.com>
2022-04-15 15:53:17 -0700

    wip fixing testing

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -2,6 +2,7 @@
 import { AgeCalc } from './src/galactic.js';
 
 describe('AgeCalc', () => {
+
   test('should return the inputted age on Earth', () => {
     const earthAge = new AgeCalc(30);
     expect(earthAge).toEqual(30);
--- src/galactic.js
+++ src/galactic.js
@@ -2,9 +2,17 @@ export class AgeCalc {
   constructor(age, lifeExpectancy) {
     this.age = age;
     this.lifeExpectancy = lifeExpectancy;
-    let planets = [('mercury', 0.24), ('venus', 0.62), ('mars', 1.88), ('jupiter', 11.86)];
+    const earth = 1;
+    const mercury = 0.24;
+    const venus = 0.62;
+    const mars = 1.88;
+    const jupiter = 11.86;
   }
   whatAge() {
-    
+    let mercuryAge = this.age * mercury;
+    let venusAge = this.age * venus;
+    let marsAge = this.age * mars;
+    let jupiterAge = this.age * jupiter;
+
   }
 }
