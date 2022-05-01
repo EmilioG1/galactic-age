de45412fd505352aefa05c9eef9028dc2144f72b
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-01 11:53:11 -0700

    add final tests and group tests for better readability

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -4,60 +4,41 @@ import AgeCalc from './../src/galactic.js';
 describe('AgeCalc', () => {
   let newAge = new AgeCalc(30);
 
-  test('should return years in earth years', () => {
+  test('should return age in planet years', () => {
     expect(newAge.earth).toEqual(30);
-  });
-
-  test('should return the inputted age on mars', () => {
-    expect(Math.floor(newAge.planetAge('mars'))).toEqual(15);
-  });
-
-  test('should return the inputted age on jupiter', () => {
-    expect(Math.floor(newAge.planetAge('jupiter'))).toEqual(2);
-  });
-
-  test('should return the inputted age on mercury', () => {
     expect(Math.floor(newAge.planetAge('mercury'))).toEqual(125);
-  });
-
-  test('should return the inputted age on venus', () => {
     expect(Math.floor(newAge.planetAge('venus'))).toEqual(48);
+    expect(Math.floor(newAge.planetAge('mars'))).toEqual(15);
+    expect(Math.floor(newAge.planetAge('jupiter'))).toEqual(2);
+  
   });
 
-  test('should return life expectancy on mercury', () => {
+  test('should return life expectancy for a specific planet', () => {
     expect(Math.floor(newAge.lifeLeft('mercury'))).toEqual(333);
-  });
-
-  test('should return life expectancy on venus', () => {
     expect(Math.floor(newAge.lifeLeft('venus'))).toEqual(129);
-  });
-
-  test('should return life expectancy on mars', () => {
     expect(Math.floor(newAge.lifeLeft('mars'))).toEqual(42);
-  });
-
-  test('should return life expectancy on jupiter', () => {
     expect(Math.floor(newAge.lifeLeft('jupiter'))).toEqual(6);
   });
 
-  test('should return final years left on mercury', () => {
+  test('should return false when planet name isnt listed', () => {
+    expect(newAge.planetAge('saturn')).toBe(false);
+    expect(newAge.lifeLeft('saturn')).toBe(false);
+    expect(newAge.finalAge('saturn')).toBe(false);
+  });
+
+  test('should return final years left on planet', () => {
     expect(Math.floor(newAge.finalAge('mercury'))).toEqual(208);
-  });
-
-  test('should return final years left on venus', () => {
     expect(Math.floor(newAge.finalAge('venus'))).toEqual(80);
-  });
-
-  test('should return final years left on mars', () => {
     expect(Math.floor(newAge.finalAge('mars'))).toEqual(26);
-  });
-
-  test('should return final years left on jupiter', () => {
     expect(Math.floor(newAge.finalAge('jupiter'))).toEqual(4);
   });
+
 
   test('should return final years left on jupiter for someone over their expected age', () => {
     let oldAge = new AgeCalc(100);
     expect(Math.ceil(oldAge.finalAge('jupiter'))).toEqual(1);
+    expect(Math.ceil(oldAge.finalAge('mars'))).toEqual(10);
+    expect(Math.ceil(oldAge.finalAge('venus'))).toEqual(32);
+    expect(Math.ceil(oldAge.finalAge('mercury'))).toEqual(83);
   });
 });
